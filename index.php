91158984e6e8b1ca469505d8f812d2a80ef1014f
logical-luke <lukasz.ziaja@chess.com>
2022-09-11 19:34:37 +0200

    :lipstick: Add static content

--- index.php
+++ index.php
@@ -1,36 +1,53 @@
 <?php
 
-declare( strict_types=1 );
+declare(strict_types=1);
 
 get_header();
 ?>
-
-    <main id="primary" class="site-main">
-
-		<?php
-		if ( have_posts() ) {
-			if ( is_home() && ! is_front_page() ) {
-				?>
-                <header>
-                    <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-                </header>
-				<?php
-			}
-
-			while ( have_posts() ) {
-				the_post();
-
-				get_template_part( 'template-parts/content', get_post_type() );
-			}
-
-			the_posts_navigation();
-		} else {
-			get_template_part( 'template-parts/content', 'none' );
-		}
-		?>
-
-    </main><!-- #main -->
-
+    <div class="tile is-ancestor">
+        <div class="tile is-vertical is-8">
+            <div class="tile">
+                <div class="tile is-parent is-vertical">
+                    <article class="tile is-child notification is-primary">
+                        <p class="title">Vertical...</p>
+                        <p class="subtitle">Top tile</p>
+                    </article>
+                    <article class="tile is-child notification is-warning">
+                        <p class="title">...tiles</p>
+                        <p class="subtitle">Bottom tile</p>
+                    </article>
+                </div>
+                <div class="tile is-parent">
+                    <article class="tile is-child notification is-info">
+                        <p class="title">Middle tile</p>
+                        <p class="subtitle">With an image</p>
+                        <figure class="image is-4by3">
+                            <img src="https://bulma.io/images/placeholders/640x480.png">
+                        </figure>
+                    </article>
+                </div>
+            </div>
+            <div class="tile is-parent">
+                <article class="tile is-child notification is-danger">
+                    <p class="title">Wide tile</p>
+                    <p class="subtitle">Aligned with the right tile</p>
+                    <div class="content">
+                        <!-- Content -->
+                    </div>
+                </article>
+            </div>
+        </div>
+        <div class="tile is-parent">
+            <article class="tile is-child notification is-success">
+                <div class="content">
+                    <p class="title">Tall tile</p>
+                    <p class="subtitle">With even more content</p>
+                    <div class="content">
+                        <!-- Content -->
+                    </div>
+                </div>
+            </article>
+        </div>
+    </div>
 <?php
-get_sidebar();
 get_footer();
