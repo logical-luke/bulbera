f462020b2583f375a1d4f43650d9e67075d8ce12
logical-luke <lukasz.ziaja@chess.com>
2022-09-11 19:33:09 +0200

    :fire: Remove footer container

--- footer.php
+++ footer.php
@@ -4,20 +4,7 @@ declare(strict_types=1);
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bulbera' ) ); ?>">
-				<?php
-				printf( esc_html__( 'Proudly powered by %s', 'bulbera' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'bulbera' ), 'bulbera', '<a href="http://www.logical-luke.com">logical-luke</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+</section>
 
 <?php wp_footer(); ?>
 
