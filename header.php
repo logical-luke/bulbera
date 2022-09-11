2bf69038503cd4ac71c0b485af4e42d44344ecaa
logical-luke <lukasz.ziaja@chess.com>
2022-09-11 20:52:58 +0200

    :lipstick: Add static favicon

--- header.php
+++ header.php
@@ -65,7 +65,7 @@ declare(strict_types=1);
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
-
+    <link rel="shortcut icon" href="<?= get_template_directory_uri() . '/images/favicons/favicon.png'?>">
     <?php wp_head(); ?>
 </head>
 
