2048889ef4f01f38a77c86398dde9411add352fa
logical-luke <lukasz.ziaja@chess.com>
2022-09-11 16:15:05 +0200

    :sparkles: Add function to get custom logo URL

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -15,3 +15,13 @@ function bulbera_body_classes( $classes ) {
 }
 
 add_filter( 'body_class', 'bulbera_body_classes' );
+
+function get_custom_logo_url(): ?string
+{
+    $custom_logo_id = get_theme_mod('custom_logo');
+    if (($image = wp_get_attachment_image_src($custom_logo_id, 'full')) && isset($image[0])) {
+        return $image[0];
+    }
+
+    return null;
+}
