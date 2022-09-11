798a5f2a95602ec010e8c54aef475280e0395939
logical-luke <lukasz.ziaja@chess.com>
2022-09-11 19:32:29 +0200

    :fire: Remove sidebar, and page container

--- page.php
+++ page.php
@@ -3,24 +3,15 @@
 declare(strict_types=1);
 
 get_header();
-?>
 
-    <main id="primary" class="site-main">
+while (have_posts()) {
+    the_post();
 
-		<?php
-		while ( have_posts() ) {
-			the_post();
+    get_template_part('template-parts/content', 'page');
 
-			get_template_part( 'template-parts/content', 'page' );
+    if (comments_open() || get_comments_number()) {
+        comments_template();
+    }
+}
 
-			if ( comments_open() || get_comments_number() ) {
-				comments_template();
-			}
-		}
-		?>
-
-    </main><!-- #main -->
-
-<?php
-get_sidebar();
 get_footer();
