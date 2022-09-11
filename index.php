c969d529009e55a5a03f235fddaecd993ba07403
logical-luke <lukasz.ziaja@chess.com>
2022-09-11 22:01:23 +0200

    :construction: Replace static content

--- index.php
+++ index.php
@@ -9,10 +9,10 @@ get_header();
             <div class="tile">
                 <div class="tile is-parent">
                     <article class="tile is-child notification is-info">
-                        <p class="title">Why is honesty most important?</p>
+                        <p class="title">Why should you automate?</p>
                         <p class="subtitle">Coming Soon</p>
                         <figure class="image is-4by3">
-                            <img src="<?= get_template_directory_uri() . '/images/placeholders/honesty.png' ?>">
+                            <img src="<?= get_template_directory_uri() . '/images/placeholders/automate.png' ?>">
                         </figure>
                     </article>
                 </div>
