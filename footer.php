6119b3937078c121167c5a0981d2ed90dc96435a
logical-luke <lukasz.ziaja@chess.com>
2022-11-18 12:10:05 +0100

    :lipstick: Adjust footer order

--- footer.php
+++ footer.php
@@ -10,14 +10,11 @@ declare(strict_types=1);
 <footer class="footer">
     <?php wp_footer(); ?>
     <div class="content has-text-centered">
-        <p>
-            Made by <a href="https://www.logical-luke.com">Logical Luke</a> 💡
-        </p>
         <p><strong>Bulbera</strong> theme provided under
             <a href="https://opensource.org/licenses/GPL-2.0">GPL v2</a> license
             is available on
             <a href="https://github.com/logical-luke/bulbera">GitHub</a>.
-
+            Made by <a href="https://www.logical-luke.com">Logical Luke</a> 💡
         </p>
         <a href="https://mentorcruise.com/mentor/ukaszziaja/"> <img src="https://cdn.mentorcruise.com/img/banner/sky-mentoring-badge.svg" width="240" alt="MentorCruise"> </a>
     </div>
