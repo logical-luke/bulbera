dda2ee8bc1b541ac762205fb6b5fdb0b2f3aadc0
logical-luke <lukasz.ziaja@chess.com>
2022-12-09 16:10:42 +0100

    :sparkles: Add 404 page

--- 404.php
+++ 404.php
@@ -4,62 +4,58 @@ declare(strict_types=1);
 
 get_header();
 ?>
+    <section class="error-404 not-found">
+        <div class="card">
+            <div class="card-image">
+                <a href="<?= get_permalink(); ?>">
+                    <?php the_post_thumbnail('main-page-thumbnail'); ?>
+                </a>
+            </div>
+            <div class="card-content">
+                <h1 class="page-title"><?php esc_html_e('Oops! That page can&rsquo;t be found.', 'bulbera'); ?></h1>
 
-    <main id="primary" class="site-main">
+                <div class="content">
+                    <p>
+                        <?php esc_html_e(
+                            'It looks like nothing was found at this location. Maybe try one of the links below or a search?',
+                            'bulbera' );
+                        ?>
+                    </p>
 
-        <section class="error-404 not-found">
-            <header class="page-header">
-                <h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'bulbera' ); ?></h1>
-            </header><!-- .page-header -->
+                    <?php
+                    get_search_form();
+                    ?>
 
-            <div class="page-content">
-                <p>
-					<?php esc_html_e(
-						'It looks like nothing was found at this location. Maybe try one of the links below or a search?',
-						'bulbera' );
-					?>
-                </p>
+                    <p>
+                    <?php
+                    the_widget( 'WP_Widget_Recent_Posts' );
+                    ?>
+                    </p>
+                    <div class="widget widget_categories">
+                        <h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'bulbera' ); ?></h2>
+                        <ul>
+                            <?php
+                            wp_list_categories(
+                                [
+                                    'orderby'    => 'count',
+                                    'order'      => 'DESC',
+                                    'show_count' => 1,
+                                    'title_li'   => '',
+                                    'number'     => 10,
+                                ]
+                            );
+                            ?>
+                        </ul>
+                    </div><!-- .widget -->
 
-				<?php
-				get_search_form();
+                    <?php
 
-				the_widget( 'WP_Widget_Recent_Posts' );
-				?>
+                    the_widget( 'WP_Widget_Tag_Cloud' );
+                    ?>
 
-                <div class="widget widget_categories">
-                    <h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'bulbera' ); ?></h2>
-                    <ul>
-						<?php
-						wp_list_categories(
-							[
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							]
-						);
-						?>
-                    </ul>
-                </div><!-- .widget -->
-
-				<?php
-				$bulbera_archive_content =
-					'<p>'
-					. sprintf(
-						esc_html__( 'Try looking in the monthly archives. %1$s', 'bulbera' ),
-						convert_smilies( ':)' )
-					)
-					. '</p>';
-				the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$bulbera_archive_content" );
-
-				the_widget( 'WP_Widget_Tag_Cloud' );
-				?>
-
-            </div><!-- .page-content -->
-        </section><!-- .error-404 -->
-
-    </main><!-- #main -->
-
+                </div>
+            </div>
+        </div>
+    </section>
 <?php
 get_footer();
