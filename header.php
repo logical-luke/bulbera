212a116fe3c7a4c196309c29444f711bc21d7936
logical-luke <lukasz.ziaja@chess.com>
2022-12-14 17:04:52 +0100

    :lipstick: Create recent posts section

--- header.php
+++ header.php
@@ -66,6 +66,8 @@ declare(strict_types=1);
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="profile" href="https://gmpg.org/xfn/11">
     <link rel="shortcut icon" href="<?= get_template_directory_uri() . '/images/favicons/favicon.png' ?>">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <?php wp_head(); ?>
 </head>
 
--- index.php
+++ index.php
@@ -4,16 +4,21 @@ declare(strict_types=1);
 
 get_header();
 ?>
-                <?php
-                    if (have_posts()) {
-                        while (have_posts()) {
-                            the_post();
-                ?>
+<?php
+if (have_posts()) {
+    ?>
+    <h2 class="title is-2 center">Recent posts</h2>
+    <div class="columns">
+        <?php
+        while (have_posts()) {
+            the_post();
+            ?>
+            <div class="column">
                 <div class="card">
                     <div class="card-image">
                         <figure class="image is-16by9">
                             <a href="<?= get_permalink(); ?>">
-                                <?php the_post_thumbnail( 'main-page-thumbnail'); ?>
+                                <?php the_post_thumbnail('main-page-thumbnail'); ?>
                             </a>
                         </figure>
                     </div>
@@ -32,45 +37,44 @@ get_header();
                             <p><a href="<?= get_permalink(); ?>">Read more</a></p>
                         </div>
                     </div>
-<!--                            <footer class="card-footer">-->
-<!--                                <a href="#" class="card-footer-item">Save</a>-->
-<!--                                <a href="#" class="card-footer-item">Edit</a>-->
-<!--                                <a href="#" class="card-footer-item">Delete</a>-->
-<!--                            </footer>-->
-                </div>
-                <?php
-                        }
-                    }
-                    else {
-                ?>
-                <div class="card">
-                    <div class="card-image">
-                        <figure class="image is-16by9">
-                            <img width="640" height="380" src="<?= get_template_directory_uri() . '/images/placeholders/to-be-revealed.png' ?>">
-                        </figure>
-                    </div>
-
-                    <div class="card-content">
-                        <p class="title">
-                            I can't wait to share details about this post.
-                        </p>
-
-                        <p class="subtitle">
-                            Coming soon!
-                        </p>
-
-                        <div class="content">
-                            There will be an exciting first post on my blog 🎉.
-                        </div>
-                    </div>
                     <!--                            <footer class="card-footer">-->
                     <!--                                <a href="#" class="card-footer-item">Save</a>-->
                     <!--                                <a href="#" class="card-footer-item">Edit</a>-->
                     <!--                                <a href="#" class="card-footer-item">Delete</a>-->
                     <!--                            </footer>-->
                 </div>
-                <?php
-                    }
-                ?>
+            </div>
+            <?php
+        }
+        ?>
+    </div>
+    <?php
+} else {
+    ?>
+    <div class="card">
+        <div class="card-image">
+            <figure class="image is-16by9">
+                <img width="640" height="380"
+                     src="<?= get_template_directory_uri() . '/images/placeholders/to-be-revealed.png' ?>">
+            </figure>
+        </div>
+
+        <div class="card-content">
+            <p class="title">
+                I can't wait to share details about this post.
+            </p>
+
+            <p class="subtitle">
+                Coming soon!
+            </p>
+
+            <div class="content">
+                There will be an exciting first post on my blog 🎉.
+            </div>
+        </div>
+    </div>
+    <?php
+}
+?>
 <?php
 get_footer();
